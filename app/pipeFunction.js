8256b44454edd6ed68a571f2f6eb216886b2cacd
Sophie Lin <moyashi1103@gmail.com>
2021-03-11 17:18:29 +0800

    Q2 added - pipe function with a variable of any type and indefinite function parameter

---
+++ app/pipeFunction.js
@@ -0,0 +1,90 @@
+/**
+Please implement a pipe function in a language that you are good at. The function parameter is of indefinite length, the first parameter is a variable of any type, and the following parameter is the function pointer. Please attach unit test.
+
+e.g.
+  def increment (int value) {
+    return value + 1
+  }
+  pipe(5, increment) => 6, pipe(5, increment, increment, increment) => 8
+*/
+
+/**
+ * addOne, addTwo, addThree, addString, addGibberish: function pointers.
+ */
+function addOne() {
+    return 1;
+}
+
+function addTwo() {
+    return 2;
+}
+
+function addThree() {
+    return 3;
+}
+
+function addString() {
+    return "addRandomStr";
+}
+
+function addGibberish() {
+    return "gibberish";
+}
+
+/**
+ * pipe: a pipe function adding commas to string.
+ * @param arg1 - a variable that could be any data type.
+ * @param {functions} func - function pointers. 
+ */
+function pipe(arg1, ...func) {
+    let result;
+    let initialValue;
+
+    // check the data type of arg1 first
+    switch (typeof arg1) {
+        // if the data type is number or string
+        case "number":
+        case "string":
+            initialValue = [arg1, ...func];
+            result = initialValue.reduce((acc, cur) => acc + cur());
+            break;
+
+        // if the data type is object
+        case "object":
+            let init;
+
+            if (!arg1.isArray) {
+                // if arg1 is an object
+                for (const key in arg1) {
+                    if (typeof init === "undefined") {
+                        init = arg1[key]
+                    } else {
+                        init += arg1[key];
+                    }
+                }
+            } else {
+                // if arg1 is an array
+                for (const i of arg1) {
+                    if (typeof init === "undefined") {
+                        init = arg1[i]
+                    } else {
+                        init += arg1[i];
+                    }
+                }
+            }
+            initialValue = [init, ...func];
+            break;
+    }
+    result = initialValue.reduce((acc, cur) => acc + cur());
+
+    return result;
+}
+
+module.exports = {
+    pipe,
+    addOne, 
+    addTwo, 
+    addThree, 
+    addString, 
+    addGibberish
+}
--- test/mochaTest.js
+++ test/mochaTest.js
@@ -1,6 +1,15 @@
 const assert = require('chai').assert;
 
 const addComma = require('../app/addComma')
+const {
+    pipe,
+    addOne, 
+    addTwo, 
+    addThree, 
+    addString, 
+    addGibberish
+} = require('../app/pipeFunction')
+
 
 // Unit test for exercise 1 - add comma to number and return string
 describe('Add comma to number', () => {
@@ -25,5 +34,29 @@ describe('Add comma to number', () => {
     it('Should remind user to enter a valid number.', () => {
         let result = addComma.numToString("This is just gibberish.");
         assert.strictEqual(result, 'Please make sure to enter a valid number.');
+    });
+})
+
+
+// Unit test for exercise 2 - pipe function with a basic variable and indefinite function parameter.
+describe('Pipe function with a base variable and indefinite function parameter', () => {
+    it('Should add numbers and strings to string', () => {
+        let result = pipe('QQ', addOne, addThree, addGibberish);
+        assert.strictEqual(result, 'QQ13gibberish');
+    });
+
+    it('Should add numbers to number', () => {
+        let result = pipe(5, addOne, addOne, addTwo);
+        assert.strictEqual(result, 9);
+    });
+
+    it('Should add numbers to an object', () => {
+        let result = pipe({a: 'aaa', b: 'bbb'}, addOne, addString, addTwo);
+        assert.strictEqual(result, 'aaabbb1addRandomStr2');
+    });
+
+    it('Should add numbers to an array', () => {
+        let result = pipe([12, 888, "qwer"], addOne, addOne, addTwo);
+        assert.strictEqual(result, '900qwer112');
     });
 })
